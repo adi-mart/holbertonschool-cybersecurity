e68a76b4f1fcda6d76ecceb40ce668eeffe1262c
adi-mmart <11535@holbertonstudents.com>
2026-08-07 17:27:11 +0200

    Add audit_groups.sh script for auditing user group memberships and 2-flag.txt for identification

---
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+awk -F: '$3 >= 1000 {print $1}' $1 | while read user; do
+for group in disk docker shadow; do
+	if id -nG "$user" | grep -wq "$group"; then
+		echo "$user:$group"
+	fi
+done
+done
