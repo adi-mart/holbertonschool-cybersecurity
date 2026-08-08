56ab94ba841c1cfdac425382fe66bdebc2786182
adi-mmart <11535@holbertonstudents.com>
2026-08-08 22:19:02 +0200

    Fix directory permissions setup in onboarding script

--- linux_security/1x02_identity_management/6-onboard.sh
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -4,7 +4,8 @@ public_key=$2
 
 useradd -m "$username"
 passwd -l "$username"
-mkdir -m 700 /home/"$username"/.ssh
+mkdir /home/"$username"/.ssh
+chmod 700 /home/"$username"/.ssh
 echo "$public_key" > /home/"$username"/.ssh/authorized_keys
 chmod 600 /home/"$username"/.ssh/authorized_keys
 chown -R "$username":"$username" /home/"$username"/.ssh
