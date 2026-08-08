4d1ae852d21b6bccb24c7aa0a90045068daa8a51
adi-mmart <11535@holbertonstudents.com>
2026-08-08 22:18:07 +0200

    Add user onboarding script for creating users with SSH access

---
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+username=$1
+public_key=$2
+
+useradd -m "$username"
+passwd -l "$username"
+mkdir -m 700 /home/"$username"/.ssh
+echo "$public_key" > /home/"$username"/.ssh/authorized_keys
+chmod 600 /home/"$username"/.ssh/authorized_keys
+chown -R "$username":"$username" /home/"$username"/.ssh
