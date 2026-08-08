a517d9e2a87ed717ba0c45154c31dbe6f230cac1
adi-mmart <11535@holbertonstudents.com>
2026-08-08 18:07:55 +0200

    Refactor SSH configuration hardening script to remove parameterized loop and fix typo in PasswordAuthentication

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 file=$1
-params=("PermitRootLogin" "PasswordAuthentication")
 
 edit_sshd_config(){
-	for PARAM in "${params[@]}"; do
-		sed -i 's/^#\?[[:space:]]*'"$PARAM"'.*/"$PARAM" no/' "$file"
-	done
+	sed -i 's/^#\?[[:space:]]*'PermitRootLogin'.*/PermitRootLogin no/' "$file"
+	sed -i 's/^#\?[[:space:]]*'PasswordAuthentication'.*/PasswordAuthenticationn no/' "$file"
 	sed -i 's/^#\?[[:space:]]*'PubkeyAuthentication'.*/PubkeyAuthentication yes/' "$file"
 }
+
 edit_sshd_config
 if sshd -t -f "$file"; then
     service ssh reload
