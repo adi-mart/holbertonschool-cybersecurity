c24ceb3767241d9dfc6ef90e9fee725d2086e7d8
adi-mmart <11535@holbertonstudents.com>
2026-08-08 19:42:56 +0200

    Fix quoting in apt-get install command in password policy script

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -2,6 +2,6 @@
 file=$2
 if ! dpkg -s "$1" 1>/dev/null 2>&1; then
 	apt-get update -y
-	apt-get install -y $1
+	apt-get install -y ${1}
 fi
 sed -i 's/^password.*pam_unix\.so.*/password requisite pam_pwquality.so minlen=12 minclass=3\n&/' "$file"
