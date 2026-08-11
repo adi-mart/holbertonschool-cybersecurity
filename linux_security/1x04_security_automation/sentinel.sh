7ab99b816df3e4d05d20922ec7e84be7ff30b2c2
adi-mmart <11535@holbertonstudents.com>
2026-08-11 11:26:00 +0200

    Add 1-flag.txt and implement check_services function in sentinel.sh for service monitoring

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 [ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILES_TO_WATCH+x}" ] || exit 1
+
+check_services() {
+	for service in "${SERVICES[@]}"; do
+		if pgrep -f "$service"; then 
+			echo "OK: $service is running"
+		else
+			eval "$service" 
+			if pgrep -f "$service"; then
+				echo "FIXED: Restarted $service"
+			else
+				echo "Error "
+			fi
+		fi
+	done 
+	}
+
+check_services
