5891762f10af62ddf648056539c75c96a36c7cef
adi-mmart <11535@holbertonstudents.com>
2026-08-11 10:10:21 +0200

    Fix variable name typos in sentinel.sh for correct service and file watching

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVCIES+x}" ] && [ -n "${ILES_TO_WATCH+x}" ] || exit 1
+[ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILES_TO_WATCH+x}" ] || exit 1
