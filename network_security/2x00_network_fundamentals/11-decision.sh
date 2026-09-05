f24ee2330617066cb94de861a4c052f333d0185f
adi-mmart <11535@holbertonstudents.com>
2026-09-06 01:49:36 +0200

    Refactor decision script to condense if-else statement into a single line

--- network_security/2x00_network_fundamentals/11-decision.sh
+++ network_security/2x00_network_fundamentals/11-decision.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if echo $(ip route get "$1") | grep -q "via"; then
-	printf "REMOTE"
-else
-	printf "LOCAL"
-fi
+if echo $(ip route get "$1") | grep -q "via"; then printf "REMOTE"; else printf "LOCAL"; fi
