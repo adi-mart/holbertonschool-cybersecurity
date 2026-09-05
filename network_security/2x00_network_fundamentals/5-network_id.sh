3c6f65a45da649662c99901e8f62bfc5baacb8d4
adi-mmart <11535@holbertonstudents.com>
2026-09-05 21:09:01 +0200

    Refactor network ID calculation script for improved readability

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-IFS='.' read -r IP1 IP2 IP3 IP4 <<< "$1"
-IFS='.' read -r MASK1 MASK2 MASK3 MASK4 <<< "$2"
-
-NET1=$((IP1 & MASK1))
-NET2=$((IP2 & MASK2))
-NET3=$((IP3 & MASK3))
-NET4=$((IP4 & MASK4))
-
-printf "%d.%d.%d.%d" "$NET1" "$NET2" "$NET3" "$NET4"
+IFS='.' read -r IP1 IP2 IP3 IP4 <<< "$1"; IFS='.' read -r MASK1 MASK2 MASK3 MASK4 <<< "$2"; NET1=$((IP1 & MASK1)); NET2=$((IP2 & MASK2)); NET3=$((IP3 & MASK3)); NET4=$((IP4 & MASK4)); printf "%d.%d.%d.%d\n" "$NET1" "$NET2" "$NET3" "$NET4"
