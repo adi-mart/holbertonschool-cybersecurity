c9a9a083f7e52c837b5110a857ec429cb8e66d90
adi-mmart <11535@holbertonstudents.com>
2026-09-11 14:07:05 +0200

    Limit output to the first matching localhost IP address in script

--- network_security/2x01_network_services/2-hosts.sh
+++ network_security/2x01_network_services/2-hosts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E '^[[:space:]]*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+[[:space:]]+.*localhost' /etc/hosts | awk '{print $1}'
+grep -E '^[[:space:]]*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+[[:space:]]+.*localhost' /etc/hosts | awk '{print $1}' | head -n 1
