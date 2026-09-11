9a08448b7cbbb66d5db8e20c27bf0cce312b569c
adi-mmart <11535@holbertonstudents.com>
2026-09-11 10:45:24 +0200

    Update DHCP script to include server identifier extraction using nmcli

--- network_security/2x01_network_services/1-dhcp.sh
+++ network_security/2x01_network_services/1-dhcp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmcli | awk '/servers/ {print $2}'
+nmcli dhcp-server-identifier|DHCP4\.OPTION.*dhcp_server_identifier|DHCPACK|server identifier|server_identifier | awk '/servers/ {print $2}'
